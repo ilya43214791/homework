6c7ae50a4972b66d8978f5bfda4e7694d50e9ce7
illua <illuazagraenko12@gmail.com>
2023-05-01 21:51:22 +0300

    updeat

--- home_numbers.py
+++ home_numbers.py
@@ -1,5 +1,6 @@
 from math import pi
+
 enter = input("Entеr please radius coolies: ")
-volume = 4/3 * round(pi,2) * float(enter)**3
+volume = 4 / 3 * (pi) * float(enter) ** 3
 around = round(volume, 2)
 print(f"Volume coolies: {around}")
